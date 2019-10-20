8a1b5aa0644d17575327bf91a25c6e5f41986cd6
Richard <richardcarvalhooo@gmail.com>
2019-10-20 12:18:35 -0300

    adding keyListener and some changes

--- src/calculadora/FrameCalc.java
+++ src/calculadora/FrameCalc.java
@@ -5,6 +5,12 @@ import javax.swing.*;
 public class FrameCalc extends JFrame {
 	private static final long serialVersionUID = 1L;
 
+	public void calcularIMC(String massa, String altura) {
+		float massaFloat = Float.parseFloat(massa);
+		float alturaFloat = (Float.parseFloat(altura)/100);
+		float imcCalculado = massaFloat / (alturaFloat * alturaFloat);			JOptionPane.showMessageDialog(null, "Seu índice de massa corporal é " + imcCalculado);
+	}
+
 	public FrameCalc(String title) {
 		super(title);
 		setLayout(new FlowLayout());
@@ -18,11 +24,11 @@ public class FrameCalc extends JFrame {
 		JPanel jpnButtons = new JPanel();
 		add(jpnButtons);
 		
-		jpnMassa.add(new JLabel("Digite sua massa: "));
+		jpnMassa.add(new JLabel("Digite sua massa (kg): "));
 		JTextField massa = new JTextField(15);
 		jpnMassa.add(massa);
 		
-		jpnAltura.add(new JLabel("Digite sua altura: "));
+		jpnAltura.add(new JLabel("Digite sua altura (cm): "));
 		JTextField altura = new JTextField(15);
 		jpnAltura.add(altura);
 		
@@ -40,17 +46,21 @@ public class FrameCalc extends JFrame {
 				JOptionPane.showMessageDialog(null, "Você não existe");
 		    } else if (massa.getText().equals("0")) {
 		    	JOptionPane.showMessageDialog(null, "Coma uma maçã");
-		    } else {
-				float massaFloat = Float.parseFloat(massa.getText());
-				float alturaFloat = (Float.parseFloat(altura.getText())/100);
-				float imcCalculado = massaFloat / (alturaFloat * alturaFloat);
-				JOptionPane.showMessageDialog(null, "Seu índice de massa corporal é " + imcCalculado);
+			} else {
+				calcularIMC(massa.getText(), altura.getText());
 			}
 		});
 		btnFechar.addActionListener((ev) -> System.exit(0)); 
 		btnLimpar.addActionListener((ev) -> {
 			massa.setText("");
 			altura.setText("");
+		});
+		altura.addKeyListener(new java.awt.event.KeyAdapter() {
+			public void keyPressed(java.awt.event.KeyEvent ev) {
+				if (ev.getKeyCode() == 10) {
+					calcularIMC(massa.getText(), altura.getText());
+				}
+			}
 		});
 	}
 }
